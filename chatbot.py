0b04e5cf763bbadb08eb9ad3179465caecfe4a6f
Zuhran Rasool <zuhranrasool308@gmail.com>
2026-07-07 11:35:31 +0500

    Add chatbot welcome screen

---
+++ chatbot.py
@@ -0,0 +1,15 @@
+# -----------------------------------------
+# AI Chatbot Project 1
+# Decode Labs AI Internship
+# Author: Zuhran Rasool
+# -----------------------------------------
+
+print("=" * 50)
+print("      Welcome to AI Rule-Based Chatbot")
+print("=" * 50)
+
+print("Hello! I am your AI Chatbot.")
+print("I can answer simple predefined questions.")
+print("Type 'help' to see available commands.")
+print("Type 'bye' anytime to exit the chatbot.")
+print("=" * 50)
